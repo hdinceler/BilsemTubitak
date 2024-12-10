075d7a37a81cb6cf322e60c2e206ad5b9ee71263
hdinceler <55253026+hdinceler@users.noreply.github.com>
2024-12-10 19:32:29 +0300

    Update ServoKontrol.ino

--- ServoKontrol.ino
+++ ServoKontrol.ino
@@ -1,32 +1,23 @@
 #define motorPin 3
 #include <Servo.h>
-
-int sicaklikDegeri = 0;  // Gelen sıcaklık değeri (0-180)
+int sicaklikDegeri=0;
 Servo motorSicaklik;
 
 void setup() {
-  motorSicaklik.attach(motorPin);  // Servo motorunu pin 3'e bağla
-  Serial.begin(9600);  // Seri haberleşmeyi başlat
+  motorSicaklik.attach(motorPin);
+  Serial.begin(9600);
 }
 
 void loop() {
-  if (Serial.available() > 0) {
-    sicaklikDegeri = Serial.parseInt();  // Seri porttan gelen değeri oku
-    
-    // Sicaklik değeri 0 ile 180 arasında olmalı
-    if (sicaklikDegeri >= 0 && sicaklikDegeri <= 180) {
-      // Sicaklik değerini mikro saniyeye dönüştür
-      int microseconds = map(sicaklikDegeri, 0, 180, 1000, 2000);
-
-      // Servo motorunu belirtilen mikro saniyeye döndür
-      motorSicaklik.writeMicroseconds(microseconds);
-      
-      // Değeri seri monitöre yazdır
-      Serial.print("Servo Pozisyonu (mikro saniye): ");
-      Serial.println(microseconds);
-    } else {
-      Serial.println("Geçersiz değer! Lütfen 0 ile 180 arasında bir değer girin.");
+ if( Serial.available()>0 ){
+  sicaklikDegeri= Serial.parseInt();
+  int mevcutKonum=motorSicaklik.read();
+  if(sicaklikDegeri>mevcutKonum){
+    for(int i=mevcutKonum; i<=sicaklikDegeri;i++ ){
+      motorSicaklik.write(sicaklikDegeri);
+      delay(20);
+      Serial.println(i);
     }
-    delay(300);  // Bir süre bekle
   }
+ }
 }
